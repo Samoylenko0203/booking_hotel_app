ccb6a49eb5aa3c366106bcee789e7936e23eb45d
Samoylenko0203 <kris_samoylenko@mail.ru>
2025-05-04 21:54:49 +0300

    update

--- app/main.py
+++ app/main.py
@@ -11,15 +11,9 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(
     lifespan=lifespan,  # Uncomment if you need to create tables on app start
-    title="Система управления задачами",
-    description="Простейшая система управления задачами, основанная на "
-                "фреймворке FastAPI.",
+    title="Система управления бронированиями в отеле",
+    description="Система управления бронированиями в отеле на фреймворке FastAPI",
     version="0.0.1",
-    contact={
-        "name": "Цифровая кафедра МФТИ",
-        "url": "https://mipt.ru",
-        "email": "digitaldepartments@mipt.ru",
-    },
     license_info={
         "name": "MIT",
         "url": "https://opensource.org/licenses/MIT",
